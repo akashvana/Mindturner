b4b28289d404619da05fe374d989771dfd8a4b8e
doomsday26 <46070017+doomsday26@users.noreply.github.com>
2020-04-13 15:50:18 +0530

    Initial Commit

---
+++ mind.py
@@ -0,0 +1,228 @@
+from datetime import datetime
+from flask import Flask,render_template,url_for,request,redirect,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from waitress import serve
+import re
+userFormat=".{5,10}"
+emailFormat="[f,h]201.{4,5}@pilani.bits-pilani.ac.in"
+phoneFormat="[0-9]{10}"
+passwordFormat=".{5,20}"
+
+from questions import *
+
+app=Flask(__name__,static_url_path='/static')
+app.config['SECRET_KEY'] = 'you-cant-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['RATELIMIT_APPLICATION'] = "4/second;12/minute"
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+limiter = Limiter(app,key_func=get_remote_address)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    flash("You have not logged in","fail")
+    return redirect(url_for('home'))
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    phone = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    stage = db.Column(db.Integer, default=0)
+    recentTime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return f"User({self.username} , {self.email} , {self.stage+1} , {self.recentTime})"
+
+@app.errorhandler(429)
+def toomanyreq(e):
+    if current_user.is_authenticated:
+        print(current_user.username+" "+current_user.email)
+    return "<h3>Too many requests!</h3><h1>Chill...</h1><h4>Try again in sometime</h4>", 429
+
+@app.route('/')
+def home():
+    if current_user.is_authenticated:
+        flash("Already Logged In",'success')
+        return redirect(url_for('question'))
+    return render_template('home.html',userFormat=userFormat,emailFormat=emailFormat,phoneFormat=phoneFormat,passwordFormat=passwordFormat)
+
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    if current_user.is_authenticated:#check if logged in
+        flash("Already Logged In",'success')
+        return redirect(url_for('question'))
+
+    if request.method == 'POST':#if form submitted
+        if not request.form.get('username') or not request.form.get('email') or not request.form.get('password') or not request.form.get('phone'):#if all details not entered
+            flash("You Must Enter All Details","fail")
+            return redirect(url_for('home'))
+
+        if not re.match(userFormat,request.form.get('username')):#check format
+            flash("Username must be 5 to 10 characters long","fail")
+            return redirect(url_for('home'))
+        if not re.match(passwordFormat,request.form.get('password')):
+            flash("Password must be 5 to 20 characters long","fail")
+            return redirect(url_for('home'))
+        if not re.match(emailFormat,request.form.get('email')):
+            flash("Email must be of BITS Mail format","fail")
+            return redirect(url_for('home'))
+        if not re.match(phoneFormat,request.form.get('phone')):
+            flash("Enter 10 digit Phone Number","fail")
+            return redirect(url_for('home'))
+
+        if User.query.filter_by(username=request.form.get('username')).first():#if user exists
+            flash("Username exists, use another or Login",'fail')
+            return redirect(url_for('home'))
+        elif User.query.filter_by(email=request.form.get('email')).first():
+            flash("Email exists, use another or Login",'fail')
+            return redirect(url_for('home'))
+        elif User.query.filter_by(phone=request.form.get('phone')).first():
+            flash("Phone number exists, use another or Login",'fail')
+            return redirect(url_for('home'))
+        else:#create new user
+            hashed_password = bcrypt.generate_password_hash(request.form.get('password')).decode('utf-8')
+            user = User(username=request.form.get('username'),email=request.form.get('email'),
+                        phone=request.form.get('phone'),password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash("Account Created Successfully!",'success')
+            login_user(user)
+            return redirect(url_for('question'))
+    flash("Enter Details First","fail")
+    return redirect(url_for('home'))
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if current_user.is_authenticated:#if logged in
+        flash("Already Logged In",'success')
+        return redirect(url_for('question'))
+
+    if request.method == 'POST':#if form submitted
+        if not request.form.get('username') or not request.form.get('password'):#if all details not entered
+            flash("You Must Enter All Details","fail")
+            return redirect(url_for('home'))
+
+        if not re.match(userFormat,request.form.get('username')):#check format
+            flash("Username must be 5 to 10 characters long","fail")
+            return redirect(url_for('home'))
+        if not re.match(passwordFormat,request.form.get('password')):
+            flash("Password must be 5 to 20 characters long","fail")
+            return redirect(url_for('home'))
+
+        user = User.query.filter_by(username=request.form.get('username')).first()
+        if user and bcrypt.check_password_hash(user.password, request.form.get('password')):#if user exists
+            login_user(user)
+            flash("Logged In Successfully",'success')
+            return redirect(url_for('question'))
+        else:#if user does not exist
+            flash("Username/Password does not match, try again or Register",'fail')
+            return redirect(url_for('home'))
+
+    flash("Enter Details First","fail")
+    return redirect(url_for('home'))
+
+
+@app.route('/solve',methods=['POST','GET'])
+@login_required
+def question():
+    if current_user.stage >= len(questionSet):
+        return redirect(url_for('finished'))
+    questions = questionSet[current_user.stage]
+    answers = answerSet[current_user.stage]
+    links = list(questions.values()).count("none")
+    if request.method == 'POST':
+        for index in range(len(questions)):
+            if list(questions.values())[index] != 'none':
+                if not request.form.get(f"question-{index+1}"):
+                    flash("You must attempt all questions",'fail')
+                    return redirect(url_for('question'))
+                elif request.form.get(f"question-{index+1}") != answers[index]:
+                    flash("One(or more) of the answers is wrong",'fail')
+                    return redirect(url_for('question'))
+        current_user.stage = current_user.stage+1
+        current_user.recentTime = datetime.now()
+        if current_user.stage < len(questionSet)-1:
+            flash(f"Stage {current_user.stage} cleared!",'success')
+        db.session.commit()
+        return redirect(url_for('question'))
+    return render_template('questionPage.html',questions=enumerate(questions.items()),
+                            stage=current_user.stage,name=current_user.username,leaderboard=getLeaderboard(),
+                            links=links,length=len(questions),images=imageSet[current_user.stage])
+
+@app.route('/solve/<link>')
+@login_required
+def special(link):
+    if current_user.stage >= len(questionSet):
+        return redirect(url_for('finished'))
+    questions = questionSet[current_user.stage]
+    answers = answerSet[current_user.stage]
+    if link in answers and list(questions.values())[answers.index(link)] == 'none':
+        current_user.stage = current_user.stage+1
+        current_user.recentTime = datetime.now()
+        if current_user.stage < len(questionSet)-1:
+            flash(f"Stage {current_user.stage} cleared!",'success')
+        db.session.commit()
+        return f'''Right answer, <a href=\"{ url_for('question') }\">continue</a><br>
+                    <img src="/static/rightmeme{ current_user.stage }.jpg">'''
+    else:
+        return f'''Wrong answer, <a href=\"{ url_for('question') }\">try again</a><br>
+                    <img src="/static/wrongmeme{ current_user.stage+1 }.jpg">'''
+
+@app.route('/congrats')
+@login_required
+def finished():
+    if current_user.stage < len(questionSet):
+        flash("You have not completed the test",'fail')
+        return redirect(url_for('question'))
+    return render_template("congrats.html",leaderboard=getLeaderboard(),name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logged Out Successfully",'success')
+    return redirect(url_for('home'))
+
+#@app.route('/reset/<stage>')
+#@login_required
+#def reset(stage):
+#    current_user.stage = int(stage)-1
+#    current_user.recentTime = datetime.now()
+#    db.session.commit()
+#    return redirect(url_for('question'))
+
+def getLeaderboard():
+    users = User.query.order_by(User.stage.desc(),User.recentTime).limit(10)
+    leaderboard = []
+    for i,user in enumerate(users):
+        stage = f"Stage {user.stage}"
+        if user.stage >= len(questionSet):
+            stage = "Finished"
+        elif user.stage == 0:
+            stage = "Started"
+        time = str(int((datetime.now()-user.recentTime).seconds/3600))+" hours ago"
+        if time[0]=='0':
+            time = str(int((datetime.now()-user.recentTime).seconds/60))+" minutes ago"
+        if time[0]=='0':
+            time = str(int((datetime.now()-user.recentTime).seconds))+" seconds ago"
+        leaderboard.append('<td>{}</td><td>: {} ,</td><td>{}</td>'.format(user.username,stage,time))
+    return leaderboard
+
+if __name__ == '__main__':
+    #app.run(debug=False,host='0.0.0.0')    #local network with debug
+    #app.run(debug=True)                    #locl machine with debug
+    serve(app,host='0.0.0.0',port=5000)     #for hosting
---
+++ questions.py
@@ -0,0 +1,85 @@
+
+stage1={    'Find the sentence enclosed within the cipher:  niwliteotbitsflesruoyghoteraperp':'text',
+            'If demon = rockstar = 100<br>Shwarzenneger = JungleBook = 200<br>Then, Avengers = ?':['125','150','175','Endgame'],
+            '''I try to open the laptop of my friend but I don\'t know the password. So, I look for the hint which is:<br>
+                A dog B cat C jimmy D corne E witty F greywolf G plateau<br>
+                Can you help me guess the password(ABCDEFG)?''':'text',
+            '''Guess the correct code with the help of the following clues(without spaces):<br>
+                a g z: Exactly one letter is perfectly placed.<br>
+                c z q: Everything is incorrect.<br>
+                d f a: Two letters are part of the code, but are wrongly placed.<br>
+                p g w: One letter is part of the code, but is wrongly placed.<br>
+                z p f: One letter is part of the code, but is wrongly placed.''':'text'
+            }
+images1=['','','','','']
+answers1=['prepare yourself to win','150','damnyou','adp']
+
+stage2={    '''Decode the following puzzles:<br>
+               1. GRAY + 'I'VORY + MAROON = ORANGE<br>
+               2. EINS + EIN'S' + EINS + DREI + DR'E'I = NEUN<br>
+               3. MINT + PE'P'PER + TOMAT'O' = CARROT<br><br>
+               Using characters(lowercase) or decoded numbers in apostrophe, fill the (space)(s) in<br>
+               https://www.bits-pilani.ac.in/Pilani/listing.aspx?c(space)d=5(space)5&(space)d=110<br><br>
+               What is the ping ip address?''':'text',
+            '''If DD = 24<br>
+                EEE = 93<br>
+                NEED = 344<br>
+                DEN = 117<br>
+                Then what is END = ?<br><br>
+                Hint: Try calculating your CG, ACBs more welcome than the rest :p''':['4.13','89','57','109'],
+            '''If 2+4^8 is 50139<br>
+                8+8/2-4 is 9094561<br>
+                18*12^4 is 8978531<br><br>
+                Then what is 21^8/4?''':['441',':-{',':-D',':\')',';-)','(¬_¬)']
+            }
+images2=['','','']
+answers2=['172.22.2.53','89',':\')']
+
+stage3={    '''Tom uses an algorithm to decide which website will he visit today. The website begins with address like Mindturner/solve/... but he doesnt remember what came after the '/'. What he does remember is the algorithm. The next letters in the link are given by the following algo:<br><br>
+	           Rule 1:   The digits in that days date (dd/m) appear in the link. One digit appears exactly once<br>
+	           Rule 2:   The rest characters are letters of the english alphabet and on rearranging always form a meaningful word<br>
+	           Rule 3:   If the date is odd, the digits take up odd places and are in the ascending order. If date is even, they are in even places and are in descending order<br>
+	           Rule 4:   The letters are always distinct<br>
+	           Rule 5:   Total no. of permutations of the link trail (considering no constraints) is 40320<br>
+	           Rule 6:   The word formed by the letters is the name of a software. Tom uses the software daily!<br>
+	           Rule 7:   Interestingly, the name is such that the letters of the proper noun when written in computer code are in the order of increasing ascii values<br>
+	           Rule 8:   The arrangement in the link is such that the letters are arranged first and the digits fill the gap. Digit may begin if the date is odd and the next gaps are compulsorily filled till we run out of digits. If date is even, the largest digit ends the link. The letters are put alphabetically<br><br>
+               Help Tom access the website.''':'none',
+               '''A long time ago in a galaxy far away, on a strange planet there were N kingdoms numbered 1 to N. Each kingdom had a jack, queen and a king who either said something good or something bad. The following is what goes on on this planet:<br><br>
+                1. At the fall of each cannon ball in the kingdom, its queen makes a statement. She would say something good if and only if:<br>
+                -- The jack said something good that day and the queen said something bad the previous day. OR<br>
+                -- The king said something bad that day and the queen said something good the previous day.<br>
+                2. Whenever the queen of a (Mth) kingdom makes a bad statement immediately after a good one, the people get angry and fire a cannon into the next(M+1 th) kingdom.<br>
+                3. Since this gives an undue advantage to kingdom 1, the evil galactic empire has placed a cannon before it which fires each day no matter what.<br><br>
+                One day a special meet was called after which the jacks and kings decided to say good things always. On this day all N queens happened to give a bad statement.<br>
+                After 4095 days, you happen to notice that all the queens are saying a good statement. What is the sum of all possible values of N?''':'none'
+            }
+images3=['','']
+answers3=['1i7L9nux','78']  #'1i7L9nux' for 17th
+                            #'i1L6n9ux' for 16th
+
+stage4={    '1 + 1 = ?<br>':['Haha','Nothing','Here'],
+            '''2 + 2 = ?<br><br>
+            Hint: Inspect Both Questions Closely  ಠ⌣ಠ''':['Still','Wondering?','Read','Hint','Again']
+            }
+images4=['','']
+answers4=['2','4']
+
+stage5={    'Thinking your entire college life is all happy and lite?<br>Bad news: Note that, only the very first part is lite, life is only darker afterwards ;<':'none'
+            }
+images5=['white.svg']
+answers5=['cglite']
+
+stage6={    'Noobs can\'t reach the end of this one:':'none'
+            }
+images6=['noob.jpg']
+answers6=['whentheredstarts']
+
+stage7={    'Guess the Indian airport':'none'
+            }
+images7=['circuit.png']
+answers7=['carnicobar']
+
+questionSet=[stage1,stage2,stage3,stage4,stage5,stage6,stage7]
+imageSet=[images1,images2,images3,images4,images5,images6,images7]
+answerSet=[answers1,answers2,answers3,answers4,answers5,answers6,answers7]
